77a0c6d02d68a96a7b4f4d6ceb4651ae514372aa
Asif Jahan <asifjahan307@gmail.com>
2024-02-29 13:50:42 +0600

    my website

--- portfolio_website/lib/changes/links.dart
+++ portfolio_website/lib/changes/links.dart
@@ -26,4 +26,4 @@ const String profileImage =
     "https://images.unsplash.com/photo-1503443207922-dff7d543fd0e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=627&q=80";
 
 // Whatsapp Link
-const String whatsapp = 'https://api.whatsapp.com/send?phone=(+880)1874392463';
+const String whatsapp = 'https://api.whatsapp.com/send?phone=01874392463';
